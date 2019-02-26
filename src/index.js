166223b93774bb8da0cd1e020d78057855bc2a56
Ilya <ilyayakimets@gmail.com>
2019-02-26 12:42:04 +0300

    add solution of triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let length = preferences.length;
+  let count = 0;
+  let result = 0;
+for(let a = 1; a < length; a++){
+   if(a=preferences[a+1]){
+      count++;
+   }
+}
+return count;
 };
